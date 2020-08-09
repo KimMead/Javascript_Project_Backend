a4c91eddcb5bed484a847f05722d55e42f63c5fa
KimMead <kimmeadows8@gmail.com>
2020-08-08 19:22:07 -0500

    add serializer gem

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,22 @@
-class QuotesController < ActionController::API
+class QuotesController < ApplicationController
 
     def index
+        quotes = Quote.all 
+        render json: quotes
+    end 
+
+    def create
+        quote = Quote.new(quote_params) 
+        if quote.save
+            render json: quote, status: :accepted
+        else
+            render json: {errors: syllabus.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end
+
+
+    private
+    def quote_params
+        params.require(:quote).permit(:content, :category_id)
     end 
 end
